1938496492d2c52c1f33e30c8d03da341a5ba326
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-12 18:50:20 +0200

    Load from GitHub yaml

--- api/index.py
+++ api/index.py
@@ -1,25 +1,16 @@
-from bs4 import BeautifulSoup
+import yaml
 import requests as req
 import re
 import json
 from http.server import BaseHTTPRequestHandler
 
-
 def getCommiters(u: str):
     country = u.split("?c=")[1].split("HTTP")[0].replace(" ", "")
-    url = f'https://commits.top/{country}.html'
+    country = "czech_republic"
+    url = f'https://raw.githubusercontent.com/lauripiispanen/github-top/master/_data/locations/{country}.yml'
     resp = req.get(url)
-    htmlGH = BeautifulSoup(resp.text, 'html.parser')
-    count = htmlGH.select(".users-list > tr")
-
-    r = []
-    for i,c in enumerate(count):
-        if i != 0:
-            children = c.findAll("td" , recursive=False)
-            s = children[1].get_text()
-            username =  s[s.find('(')+1:s.find(')')]
-            r.append({"rank": children[0].get_text().replace(".",""), "user": {"name": username, "username": s.split(" ")[0]}, "contributors": children[2].get_text()})
-    return '{"users": '+json.dumps(r)+"}"
+    y = yaml.safe_load(resp.text)
+    return '{"users": '+json.dumps(y)+"}"
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
