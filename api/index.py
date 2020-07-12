209c1b3eab2c9dccf8e6e1f60dc847abbb16c98a
Restyled.io <commits@restyled.io>
2020-07-12 16:51:07 +0000

    Restyled by autopep8

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ import re
 import json
 from http.server import BaseHTTPRequestHandler
 
+
 def getCommiters(u: str):
     country = u.split("?c=")[1].split("HTTP")[0].replace(" ", "")
     country = "czech_republic"
@@ -12,10 +13,11 @@ def getCommiters(u: str):
     y = yaml.safe_load(resp.text)
     return '{"users": '+json.dumps(y)+"}"
 
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','application/json')
+        self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         message = getCommiters(self.requestline)
