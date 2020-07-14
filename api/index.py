46384af38c3f0951d5703c2e2e316d276ea4f29d
Restyled.io <commits@restyled.io>
2020-07-14 16:24:11 +0000

    Restyled by autopep8

--- api/index.py
+++ api/index.py
@@ -7,7 +7,8 @@ import yaml
 
 
 def getCommiters(u: str):
-    splitRequestLine = (u.split("?c=")[1].split("HTTP")[0].replace(" ", "")).split("&v=")
+    splitRequestLine = (u.split("?c=")[1].split(
+        "HTTP")[0].replace(" ", "")).split("&v=")
     country = splitRequestLine[0]
     visibility = splitRequestLine[1]
     url = f"https://raw.githubusercontent.com/lauripiispanen/github-top/master/_data/locations/{country}.yml"
@@ -15,13 +16,12 @@ def getCommiters(u: str):
 
     if resp.status_code == 200:
         y = yaml.safe_load(resp.text)
-        #return u
+        # return u
         return '{"users": ' + json.dumps(y) + "}"
     elif resp.status_code == 404:
         return ('{"message": "Location not found. Take a look at /locations for the available locations"}')
     elif resp.status_code == 500:
         return ('{"message": "An error occurred. A server one. Try again later, I guess."}')
-
 
 
 class handler(BaseHTTPRequestHandler):
